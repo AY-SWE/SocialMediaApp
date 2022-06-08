627da8a498ae2f282a5221bbd1f537626aea83e8
Andy Yang <yangandy1001@gmail.com>
2022-06-07 22:19:16 -0400

    finished adding share image/video functionality in postShare

--- client/src/components/postShare/PostShare.jsx
+++ client/src/components/postShare/PostShare.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import "./PostShare.scss"
 import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
 import VideoCameraBackOutlinedIcon from '@mui/icons-material/VideoCameraBackOutlined';
 import AddLocationOutlinedIcon from '@mui/icons-material/AddLocationOutlined';
 import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import CloseIcon from '@mui/icons-material/Close';
 
 const PostShare = () => {
+    const[image, setImage] = useState(null);
+    const imageRef = useRef();
+    const onImageChange =(e)=>{
+        if(e.target.files && e.target.files[0]){
+            let img = e.target.files[0];
+            setImage({
+                image: URL.createObjectURL(img)
+            });
+        }
+    };
+ 
+
   return (
     <div className="postShare">
         <img src="https://www.themarysue.com/wp-content/uploads/2022/05/Anya-smile.jpg" alt=''/>
@@ -13,24 +26,45 @@ const PostShare = () => {
         <div className='whatHappening'>
             <input type="text" placeholder="What is happening" />
         
-        <div className="postOptions">
-            <div className="option">
-                <AddPhotoAlternateOutlinedIcon/>
-                Photo
+            <div className="postOptions">
+                <div className="option"
+                style={{color: "var(--photo)"}}
+                onClick ={()=> imageRef.current.click()}>
+                    <AddPhotoAlternateOutlinedIcon/>
+                    Photo
+                </div>
+                <div className="option"
+                style={{color: "var(--video)"}}
+                >
+                    <VideoCameraBackOutlinedIcon/>
+                    Video
+                </div>
+                <div className="option"
+                style={{color: "var(--location)"}}>
+                    <AddLocationOutlinedIcon/>
+                    Location
+                </div>
+                <div className="option"
+                style={{color: "var(--schedule)"}}>
+                    <CalendarMonthOutlinedIcon/>
+                    Schedule
+                </div>
+
+                <button className="button-postShare">
+                    Share
+                </button>
+
+                <div style={{display:"none"}}>
+                    <input type="file" name= "myImage" ref={imageRef} onChange={onImageChange}/>
+                </div>
             </div>
-            <div className="option">
-                <VideoCameraBackOutlinedIcon/>
-                Video
-            </div>
-            <div className="option">
-                <AddLocationOutlinedIcon/>
-                Location
-            </div>
-            <div className="option">
-                <CalendarMonthOutlinedIcon/>
-                Schedule
-            </div>
-        </div>
+
+            {image && (
+                <div className="previewImage">
+                     <CloseIcon onClick={()=> setImage(null)}/>
+                     <img src={image.image} alt=""/>
+                </div>  
+            )}
         </div>
     </div>
   )
