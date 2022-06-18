e074e29b96352b95e285436a1129769ec84f245f
Andy Yang <yangandy1001@gmail.com>
2022-06-17 21:56:47 -0400

    like/dislike a post finished, works well

--- client/src/api/postRequestApi.js
+++ client/src/api/postRequestApi.js
@@ -6,3 +6,4 @@ const api = axios.create({
 
 
 export const getTimelinePost = (id) => api.get(`/post/findTimeLine/${id}`)
+export const likeDislikePost = (id, userId) => api.put(`/post/likeDislike/${id}`, {userId: userId}) //userId will be in the req.body, id will be req.params.id, matches server side 
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -5,19 +5,31 @@ import Heart from '../../img/like.png'
 import NotLike from '../../img/notlike.png'
 import "./Post.scss"
 import { useSelector } from 'react-redux'
+import { useState } from 'react'
+import { likeDislikePost } from '../../api/postRequestApi'
+
 const Post = ({data}) => {
   const user = useSelector((state)=>state.authReducer.authData.user.existingUser);
+  const [liked, setliked] = useState(data.likes.includes(user._id));
+  const [likes, setLikes] = useState(data.likes.length);
+
+  const handleLike = () => {
+      setliked((prev) => !prev);
+      likeDislikePost(data._id, user._id)
+      liked? setLikes((prev) => prev - 1): setLikes((prev) => prev + 1)
+  }
+
   return (
     <div className="post">
       <img src={data.img? process.env.REACT_APP_PUBLIC_FOLDER + data.img: ""} alt=""/>
 
       <div className="postReact">
-          <img src= {data.liked? Heart: NotLike} alt=""/>
+          <img src= {liked? Heart: NotLike} alt="" onClick={handleLike}/>
           <img src= {Comment} alt=""/>
           <img src= {Share} alt=""/>
       </div>
 
-      <span style={{color:"black", fontSize:"13px"}}>{data.likes} Likes</span>
+      <span style={{color:"black", fontSize:"13px"}}>{likes} Likes</span>
       <div className="details">
         <span><b>{data.name}</b></span>
         <span> {data.description}</span>
